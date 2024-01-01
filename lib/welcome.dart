e4e0bbb71effbf8e593e865e9ab21381b96afd76
Ali Kozaiha <alikozaiha073@gmail.com>
2024-01-01 15:28:06 +0200

    cange w

--- lib/welcome.dart
+++ lib/welcome.dart
@@ -14,137 +14,80 @@ class _WelcomeState extends State<Welcome> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
-      backgroundColor: Colors.white,
-      body: Stack(
-        children: [
-          Positioned.fill(
-            child: Container(
+      body:
+        Row(children: [
+          Column(children: [
+            Container(width: 195,height: 803,
               decoration: BoxDecoration(
                 gradient: LinearGradient(
-                  colors: [Colors.purple.shade100, Colors.white],
-                  begin: Alignment.topCenter,
-                  end: Alignment.bottomCenter,
+                  colors: [Colors.black, Colors.red, Colors.yellow],
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
                 ),
               ),
-            ),
-          ),
-          Positioned.fill(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                SizedBox(height: 100),
-                Text(
-                  "Welcome to Lottie",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 45,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.purple,
-                    shadows: [
-                      Shadow(
-                        blurRadius: 5.0,
-                        color: Colors.black.withOpacity(0.3),
-                        offset: const Offset(4.0, 4.0),
+              child: Column(
+                children: [
+                  SizedBox(height: 401,),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(backgroundColor: Colors.white,shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)))
+                    ,onPressed: () {
+                    Navigator.push(context, MaterialPageRoute(builder: (context) => pagetwo(),));
+                  },
+                    child: Container(width: 100,height: 50,
+
+                      child: Center(
+                        child: Text(
+                          "Germany",
+                          style: TextStyle(
+                            fontSize: 20,
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
                       ),
-                    ],
-                  ),
-                ),
-                SizedBox(height: 50),
-                Container(
-                  height: 350,
-                  margin: EdgeInsets.symmetric(horizontal: 20),
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Lottie.network(
-                      "https://lottie.host/425d7d3a-618a-4bf6-a059-8c078e22ce0b/At0L2hWX8n.json",
                     ),
                   ),
-                ),
-                SizedBox(height: 40),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      Expanded(
-                        flex: 1,
-                        child: GestureDetector(
-                          onTap: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => pageone(),
-                              ),
-                            );
-                          },
-                          child: Container(
-                            padding: EdgeInsets.all(20),
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(12),
-                              gradient: LinearGradient(
-                                colors: [Colors.green, Colors.white, Colors.red],
-                                begin: Alignment.topLeft,
-                                end: Alignment.bottomRight,
-                              ),
-                            ),
-                            child: Center(
-                              child: Text(
-                                "Italy",
-                                style: TextStyle(
-                                  fontSize: 20,
-                                  color: Colors.black,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                            ),
-                          ),
-                        ),
-                      ),
-                      SizedBox(width: 20),
-                      Expanded(
-                        flex: 1,
-                        child: GestureDetector(
-                          onTap: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => pagetwo(),
-                              ),
-                            );
-                          },
-                          child: Container(
-                            padding: EdgeInsets.all(20),
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(12),
-                              gradient: LinearGradient(
-                                colors: [Colors.black, Colors.red, Colors.yellow],
-                                begin: Alignment.topLeft,
-                                end: Alignment.bottomRight,
-                              ),
-                            ),
-                            child: Center(
-                              child: Text(
-                                "Germany",
-                                style: TextStyle(
-                                  fontSize: 20,
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
+                ],
+              ),
+
+
             ),
-          ),
-        ],
-      ),
-    );
+          ],),
+          Column(children: [
+            Container(width: 197,height: 803,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [Colors.green, Colors.white, Colors.red],
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                ),
+              ),
+              child: Column(
+                children: [
+                  SizedBox(height: 401,),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(backgroundColor: Colors.white,shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)))
+                    ,onPressed: () {
+                    Navigator.push(context, MaterialPageRoute(builder: (context) => pageone(),));
+                  },
+                    child: Container(width: 100,height: 50,
+
+                      child: Center(
+                        child: Text(
+                          "Italy",
+                          style: TextStyle(
+                            fontSize: 20,
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],)
+        ],));
   }
 }
